d84f5579db0a3e0468049fc5e503319e834e683f
bahador <noor.bahadory786@gmail.com>
2024-04-08 17:55:18 +0100

    completd getting data from movies.json

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
-const e = require("express");
+//const e = require("express");
+const fs = require("fs").promises;
 const express = require("express");
 const app = express();
-app.use(express.json());
-const movies = [
-  {
-    albumId: "10",
-    artistName: "Beyoncé",
-    collectionName: "Lemonade",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
-    releaseDate: "2016-04-25T07:00:00Z",
-    primaryGenreName: "Pop",
-    url: "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-  {
-    albumId: "11",
-    artistName: "Beyoncé",
-    collectionName: "Dangerously In Love",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
-    releaseDate: "2003-06-24T07:00:00Z",
-    primaryGenreName: "Pop",
-    url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-];
+//app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("you are listening in server 3000");
-  res.send("this is root server!");
-});
+// app.get("/", (req, res) => {
+//   //read movies data from movies.json
+//   fs.readFile("../movies.json", "utf8").then((movies) => console.log(movies));
+//   res.send({ movies });
+// });
+
+// app.get("/movies", (req, res) => {
+//   console.log("hitting the movies endpoint");
+//   //read movies data from movies.json
+//   fs.readFile("./movies.json", "utf8").then((movies) => {
+//     //change string into object
+//     movies = JSON.parse(movies);
+//     console.log({ movies });
+//     res.status(200).send({ movies });
+//   });
+// });
 
 app.get("/movies", (req, res) => {
   console.log("hitting the movies endpoint");
-  res.status(200).send({ movies });
+  // Read movies data from movies.json
+  fs.readFile("./movies.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return res.status(500).send({ error: "Failed to read movies data" });
+    }
+
+    try {
+      // Parse the JSON string into a JavaScript object
+      const movies = JSON.parse(data);
+      // Log movies data to console (for debugging)
+      console.log({ movies });
+      // Send movies data as a response with HTTP status 200 (OK)
+      res.status(200).send({ movies });
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      res.status(500).send({ error: "Failed to parse movies data" });
+    }
+  });
 });
 
 app.post("/movies", (req, res) => {
@@ -41,10 +50,59 @@ app.post("/movies", (req, res) => {
   res.status(201).send({ newMovies });
 });
 
-app.get("/movie/:movieId", (req, res) => {
+app.get("/movies/:movieId", (req, res) => {
   console.log("you are listening to dynamic server");
   const id = req.params.movieId;
   const movieArray = movies.find((movie) => movie.albumId === id);
   res.send(movieArray);
 });
-app.listen(30001);
+
+app.put("/movies/:id", (req, res) => {
+  console.log("hitting put endpoint...");
+  const albumId = req.params.id;
+  const updatedMovieData = req.body;
+  const movieIndex = movies.findIndex((movie) => movie.albumId === albumId);
+
+  if (movieIndex === -1) {
+    return res.status(404).send({
+      status: false,
+      Message: "Movie not found",
+    });
+  }
+  //get the old version
+  const oldMovies = { ...movies[movieIndex] };
+  const updatedMovie = { ...movies[movieIndex], ...updatedMovieData };
+  movies.splice(movieIndex, 1, updatedMovie);
+  //console.log("new movies-->", newMovies);
+  res.status(200).send({
+    status: true,
+    message: "Movie updated successfully",
+    oldMovie: oldMovies,
+    updatedMovie: updatedMovie,
+  });
+});
+
+// app.put("/movies/:id", (req, res) => {
+//   const albumId = req.params.id;
+//   console.log("id route -->", albumId);
+//   const movieIndex = movies.findIndex((movie) => movie.albumId === albumId);
+//   console.log("movie index first--->", movieIndex);
+
+//   if (movieIndex === -1) {
+//     return res.status(404).send({ status: false, message: "Movie not found" });
+//   }
+//   // get the old version
+//   const oldMovie = { ...movies[movieIndex] };
+
+//   const updatedMovie = { ...movies[movieIndex], ...req.body };
+//   movies[movieIndex] = updatedMovie;
+//   res.status(200).send({
+//     status: true,
+//     message: "Movie updated successfully",
+//     oldMovie: oldMovie,
+//     movie: updatedMovie,
+//   });
+//   console.log("movie index -->", movieIndex);
+// });
+
+app.listen(3001);
